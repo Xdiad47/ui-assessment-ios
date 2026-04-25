f39efe85cba07ec1c322dc462bcc246a7ca3c154
Xdiad47 <mail2diadem@gmail.com>
2026-04-25 18:51:10 +0530

    Initial Commit

---
+++ Itzeazy/ItzeazyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItzeazyApp.swift
+//  Itzeazy
+//
+//  Created by Diadem nath on 25/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ItzeazyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
